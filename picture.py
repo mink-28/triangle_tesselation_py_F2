3c4ece27b358a02a23dddbbe9607dae9947b4d0d
jaylee1056 <yoonjunglee29@ssis.edu.vn>
2026-05-20 14:51:48 +0700

    I did blue triangles
    Added comments and modified fill_triangle parameters for blue triangles.

--- picture.py
+++ picture.py
@@ -26,8 +26,11 @@ def draw_picture(width, height):
         sg.fill_triangle(2*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 3*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 3*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT) 
         
     
-    # code for BLUE triangles
+    # code for BLUE triangles  #Jay
+     sg.set_fill_color(colors[2]) # set fill color to red
     
+    for i in range(5):   # fill_triangle(x1, y1, x2, y2, x3, y3)
+        sg.fill_triangle(2*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 3*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT,2*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT)
     
     # code for YELLOW triangles
    
